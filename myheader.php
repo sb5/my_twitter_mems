6073bfaf72cf8f20697e1d1f9ac0c76dbd4f4072
sb5 <steveb533@hotmail.com>
2013-06-07 22:27:02 -0300

    linked stylesheet

--- myheader.php
+++ myheader.php
@@ -1,27 +1,24 @@
 <head>
-<!DOCTYPE html>
+    <!DOCTYPE html>
 <html lang="en">
-<meta charset="utf-8">
-<!--site by Stephen Breighner special thanks to Twitter Bootstrap!-->
+    <meta charset="utf-8">
+    <!--site by Stephen Breighner!-->
 
 
 
-<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
 
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?php 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php
+        $title = $_SERVER['PHP_SELF'];
+        $title = str_replace('/', '', $title);
+        $title = explode('.', $title);
+        $title = Ucfirst($title[0]);
 
-$title = $_SERVER['PHP_SELF'];
-$title = str_replace('/','',$title);
-$title = explode('.',$title);
-$title = Ucfirst($title[0]);
-
-echo $title; ?> | Stephen Breighner Photography</title>
-
-
-<style>body{background-image: url(http://1-ps.googleusercontent.com/h/www.javascriptbank.com/javascript/multi…tar_Background/xxstars.jpg,Mic.B4PzxN4m8R.jpg.pagespeed.ic.b6s-A1Ud0e.webp);margin: 20px;font-family: helvetica, arial, serif; color: rgb(100, 100, 100);
-}h2{font-family: zapfino;color: black;}h3{font-family: serif;font-size: small;color: black;}.tweets, #img{text-align: center; }</style>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+        echo $title;
+        ?> | Stephen Breighner Photography</title>
+    <link type ="text/css" href="tm_style.css" media="all" rel="stylesheet"/>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
 
 
 
