730d0415ad5dba9cda3868b5f2912d1abc3b9d32
sb5 <steveb533@hotmail.com>
2013-06-07 21:53:27 -0300

    got rid of spaces

--- my_tweets.php
+++ my_tweets.php
@@ -4,30 +4,19 @@ include 'myheader.php';
 echo '<h2 style = "text-shadow: 2px 2px 2px #CCCCCC;">My Twitter Memories</h2><h3 class = "muted" ><em>are you thinking of leaving the twiter world? Me too..anyways, maybe you should grab all of your tweets before u go?</em></h6>';
 if (isset($_GET['name'])) {
     $name = htmlentities($_GET['name']);
-
     $url = "https://api.twitter.com/1/statuses/user_timeline/$name.json?callback=?&count=2000";
-
     echo '<div id ="img"></div><div class = "tweets"></div>';
     ?><script>
             $(document).ready(function() {
                 loadTweets();
             });
-
             function loadTweets() {
                 var _url = <?php echo "'" . $url . "'"; ?>;
-
                 $.getJSON(_url, function(data) {
-
-
-
                     var profileImg = data[0].user.profile_image_url_https;
-
                     $("#img").html("<img src = '" + profileImg + "'  alt = ''>");
                     for (i = 0; i < data.length; i++) {
-
-
                         var tweet = data[i].text;
-
                         $(".tweets").append('<p id = "' + i + '">' + tweet + '</p>');
                     }
                 });
