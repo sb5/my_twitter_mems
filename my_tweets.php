62c80c613999f4e2e229df1cd9609439b8bb64a8
sb5 <steveb533@hotmail.com>
2013-06-07 20:50:33 -0400

    got rid of some pointless stuff

--- my_tweets.php
+++ my_tweets.php
@@ -7,42 +7,33 @@ if (isset($_GET['name'])) {
 
     $url = "https://api.twitter.com/1/statuses/user_timeline/$name.json?callback=?&count=2000";
 
-    $curl_handle = curl_init();
-    curl_setopt($curl_handle, CURLOPT_URL, $url);
-    curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
-    curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl_handle, CURLOPT_SSL_VERIFYPEER, false);
-    $f = curl_exec($curl_handle);
-    curl_close($curl_handle);
-
-
     echo '<div id ="img"></div><div class = "tweets"></div>';
     ?><script>
-        $(document).ready(function() {
-            loadTweets();
-        });
-
-        function loadTweets() {
-            var _url = <?php echo "'" . $url . "'"; ?>;
-
-            $.getJSON(_url, function(data) {
-
-
-
-                var profileImg = data[0].user.profile_image_url_https;
-
-                $("#img").html("<img src = '" + profileImg + "'  alt = ''>");
-                for (i = 0; i < data.length; i++) {
-
-
-                    var tweet = data[i].text;
-
-                    $(".tweets").append('<p id = "' + i + '">' + tweet + '</p>');
-                }
+            $(document).ready(function() {
+                loadTweets();
             });
-        }
+
+            function loadTweets() {
+                var _url = <?php echo "'" . $url . "'"; ?>;
+
+                $.getJSON(_url, function(data) {
+
+
+
+                    var profileImg = data[0].user.profile_image_url_https;
+
+                    $("#img").html("<img src = '" + profileImg + "'  alt = ''>");
+                    for (i = 0; i < data.length; i++) {
+
+
+                        var tweet = data[i].text;
+
+                        $(".tweets").append('<p id = "' + i + '">' + tweet + '</p>');
+                    }
+                });
+            }
     </script>
-<?php
+    <?php
 } else {
     echo '<br><form action = "" method = "get"><span class="add-on">@</span>
   <input class="span2" id="prependedInput" type="text" placeholder="Username" name = "name"><input type  ="submit" value = "submit"/></form>
